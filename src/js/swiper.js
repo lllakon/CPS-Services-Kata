ff19fa07e0c3518f6b1ec4c891cfbc1fdde01309
lllakon <CoffeWhite58@gmail.com>
2024-09-05 18:45:06 +0200

    -
    -

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -7,7 +7,7 @@ function detectMobileDevice() {
   if (
     /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
       navigator.userAgent
-    ) &&
+    ) && window.innerWidth <= 550 ||
     window.innerWidth <= 550
   ) {
     // mobile device
